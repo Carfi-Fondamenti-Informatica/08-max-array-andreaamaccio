f0e613c3b1601193da31a4577c4921a20c1fa4ad
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-12-09 14:07:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int dimensione;
+    cin>>dimensione;
+    float array[dimensione];
+    for (int i=0; i<dimensione; i++){
+        cin>>array[i];
+    }
+    cout<<maggiore(array, dimensione);
 }
