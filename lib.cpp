9874b0345e5f85f8f1854b9c700d3e1df5a16fd1
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-12-09 14:05:24 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "lib.h"
 
+int maggiore(int vettore[], int n)
+{
+    int i=0;
+    while(i<n){
+    if (vettore[0]>vettore[i])
+        i++;
+    else{
+        vettore[0]=vettore[i];
+        i++;
+    }
+    }
+    return vettore[0];
+}
